8415b13a74a752dd7e629d30656588ef0f764589
MikhailSobhy <mikhailsobhy66@gmail.com>
2025-04-05 14:20:42 +0200

    second commit

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Error() {
+  return <>
+<div className='w-full md:w-[80%] mx-auto md:h-[200px] h-[100px] md:py-5 md:px-10 py-3 px-6 bg-gray-200 text-sm md:text-2xl items-center flex justify-center'>Error, Not Found Page</div>
+  </>
+}
